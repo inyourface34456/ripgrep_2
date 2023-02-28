7a368ebf9ed801d9845355b52c40d554d17715fd
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-28 19:33:37 +0000

    fixed it

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,28 @@
 use std::{env, fs, process};
 
-struct Config {
-    query: String,
-    file_path: String,
+struct Config<'a> {
+    query: &'a str,
+    file_path: &'a str,
 }
 
-impl Config {
-    fn parce_args(args: &[String]) -> Self {
+impl<'a> Config<'a> {
+    fn parce_args(args: &'a [String]) -> Self {
         Self {
-            query: args[2],
-            file_path: args[1],
+            query: &args[2],
+            file_path: &args[1],
         }
     }
 
-    fn read_file() {
-        
+    fn read_file(&self) -> String {
+        match fs::read_to_string(&self.file_path) {
+            Err(e) => {
+                eprintln!("An error has ocurred ({}).", e);
+                process::exit(2);
+            },
+            Ok(o) => o,
+        }
     }
 }
-
-/*fn parce_args(args: &[String]) -> Config {
-    let query = &args[2];
-    let file = &args[1];
-
-    (file, query)
-}
-
-fn content_fetcher(path: &str) -> String {
-    match fs::read_to_string(path) {
-        Err(e) => {
-            eprintln!("An error has ocurred ({}).", e);
-            process::exit(2);
-        },
-        Ok(o) => o,
-    }
-}*/
 
 fn main() {
     let arg = env::args().collect::<Vec<String>>();
@@ -41,16 +30,13 @@ fn main() {
     if arg.len() != 3 {
         eprintln!("You must provide 3 arguments.");
     } else {
-        let (file, query) = parce_args(&arg);
+        let args = Config::parce_args(&arg);
        
 
-        println!("Searching for {} in file {}", query, file);
+        println!("Searching for {} in file {}", args.query, args.file_path);
 
-        let contents= content_fetcher(file);
-        };
+        let contents= args.read_file();
 
         dbg!("The file has the contents {}", contents);
     }
-
-    
 }
