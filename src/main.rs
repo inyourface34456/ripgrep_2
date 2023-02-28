a71597c1bad5e725a235b3a9d3f4ebce096c7566
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-28 17:56:07 +0000

    yep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    panic!("to the disco!");
+    let arg = env::args().collect::<Vec<String>>();
+
+    if arg.len() != 3 {
+        eprintln!("You must provide 3 arguments.");
+    } else {
+        let file = &arg[1];
+        let query = &arg[2];
+
+        println!("Searching for {} in file {}", query, file);
+    }
+
+    
 }
