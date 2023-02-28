97bbc5a8eeca634130f9db85e63cfd781c3dc3b3
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-28 19:39:51 +0000

    oops

--- src/main.rs
+++ src/main.rs
@@ -31,9 +31,9 @@ impl<'a> Config<'a> {
 
 fn main() {
     let arg = env::args().collect::<Vec<String>>();
-
-        let args = Config::parce_args(&arg);
-        println!("Searching for {} in file {}", args.query, args.file_path);
-        let contents= args.read_file();
-        dbg!("The file has the contents {}", contents);
+    
+    let args = Config::parce_args(&arg);
+    println!("Searching for {} in file {}", args.query, args.file_path);
+    let contents= args.read_file();
+    dbg!("The file has the contents {}", contents);
 }
