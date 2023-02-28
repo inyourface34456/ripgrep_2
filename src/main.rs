1151e1b7a660bd0b6386f815bdff5d7bd210e2a0
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-28 20:33:04 +0000

    more extreame refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
-use std::{env, fs, process};
-
-struct Config<'a> {
-    query: &'a str,
-    file_path: &'a str,
-}
-
-impl<'a> Config<'a> {
-    fn parce_args(args: &'a [String]) -> Self {
-        if args.len() != 3 {
-            eprintln!("There must be exatly 3 arguments.");
-            process::exit(2)
-        } else {
-            Self {
-                query: &args[2],
-                file_path: &args[1],
-            }
-        }
-    }
-
-    fn read_file(&self) -> String {
-        match fs::read_to_string(&self.file_path) {
-            Err(e) => {
-                eprintln!("An error has ocurred ({}).", e);
-                process::exit(2);
-            },
-            Ok(o) => o,
-        }
-    }
-}
+mod utils;
+use utils::*;
+use std::env;
 
 fn main() {
     let arg = env::args().collect::<Vec<String>>();
---
+++ src/utils.rs
@@ -0,0 +1,28 @@
+use std::{env, fs, process};
+
+
+pub struct Config<'a> {
+    pub query: &'a str,
+    pub file_path: &'a str,
+}
+
+impl<'a> Config<'a> {
+    pub fn parce_args(args: &'a [String]) -> Self {
+        if args.len() != 3 {
+            eprintln!("There must be exatly 3 arguments.");
+            process::exit(2)
+        } else {
+            Self {
+                query: &args[2],
+                file_path: &args[1],
+            }
+        }
+    }
+
+    pub fn read_file(&self) -> String {
+        fs::read_to_string(&self.file_path).unwrap_or_else(|err| {
+            eprintln!("An error has ocurred ({})", err);
+            process::exit(2);
+        })
+    }
+}
