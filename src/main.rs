84483d4ca3d87c97467733ad6eeeb77817f44115
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-28 18:15:14 +0000

    added some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,39 @@
-use std::env;
+use std::{env, fs, process};
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn parce_args(args: &[String]) -> Self {
+        Self {
+            query: args[2],
+            file_path: args[1],
+        }
+    }
+
+    fn read_file() {
+        
+    }
+}
+
+/*fn parce_args(args: &[String]) -> Config {
+    let query = &args[2];
+    let file = &args[1];
+
+    (file, query)
+}
+
+fn content_fetcher(path: &str) -> String {
+    match fs::read_to_string(path) {
+        Err(e) => {
+            eprintln!("An error has ocurred ({}).", e);
+            process::exit(2);
+        },
+        Ok(o) => o,
+    }
+}*/
 
 fn main() {
     let arg = env::args().collect::<Vec<String>>();
@@ -6,10 +41,15 @@ fn main() {
     if arg.len() != 3 {
         eprintln!("You must provide 3 arguments.");
     } else {
-        let file = &arg[1];
-        let query = &arg[2];
+        let (file, query) = parce_args(&arg);
+       
 
         println!("Searching for {} in file {}", query, file);
+
+        let contents= content_fetcher(file);
+        };
+
+        dbg!("The file has the contents {}", contents);
     }
 
     
